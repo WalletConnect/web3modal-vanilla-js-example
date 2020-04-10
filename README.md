e841fe232bffe285a23b1d1737c54667b9456f1a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2020-04-10 23:45:19 +0200

    Almost fully functional

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-This is a vanille JavaScript example how to use Web3modal with different wallet providers.
+This is a vanilla JavaScript example how to use Web3modal with different wallet providers.
 
 * View demo
 * Open a live example in Gitpod online editor to hack
 
 Ethereum node for the example provided by [QuickNode.io](https://quicknode.io)
+
+# Local development
+
+Because this example uses [ES6 modules](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import) you cannot
+open the `index.html` file from your local filesystem. Instead you need to open it through a development.
+
+One example is using Python 3. Install Python 3, then do:
+
+```sh
+# Python command might be python or python3 depending on the installation
+python3 -m http.server
+```
+
+... in the folder of index.html file.
--- example.js
+++ example.js
@@ -1,8 +1,36 @@
-window.addEventListener('load', async () => {
-  const Web3Modal = window.Web3Modal.default;
+"use strict";
 
-  const WalletConnectProvider = window.WalletConnectProvider.default;
+/**
+ * Example JavaScript code that interacts with the page and Web3 wallets
+ */
 
+ // Unpkg imports
+const Web3Modal = window.Web3Modal.default;
+const WalletConnectProvider = window.WalletConnectProvider.default;
+const EvmChains = window.EvmChains;
+
+// Web3modal instance
+let web3Modal
+
+// Chosen wallet provider given by the dialog window
+let provider;
+
+
+// Address of the selected account
+let selectedAccount;
+
+
+/**
+ * Setup the orchestra
+ */
+function init() {
+
+  console.log("Initializing example");
+  console.log("WalletConnectProvider is", WalletConnectProvider);
+
+  // Tell Web3modal what providers we have available.
+  // Built-in web browser provider (only one can exist as a time)
+  // like MetaMask, Brave or Opera is added automatically by Web3modal
   const providerOptions = {
     walletconnect: {
       package: WalletConnectProvider,
@@ -15,40 +43,109 @@ window.addEventListener('load', async () => {
     }
   };
 
-  const web3Modal = new Web3Modal({
+  web3Modal = new Web3Modal({
     network: "mainnet", // optional
-    cacheProvider: true, // optional
+    cacheProvider: false, // optional
     providerOptions // required
   });
 
-  // Get a live wallet provider
-  const provider = await web3Modal.connect();
+}
+
+
+/**
+ * Kick in the UI action after Web3modal dialog has chosen a provider
+ */
+async function fetchAccountData(provider) {
 
   // Get a Web3 instance for the wallet
   const web3 = new Web3(provider);
 
+  console.log("Web3 is", web3);
+  console.log(EvmChains);
+
+  // Get connected chain id from Ethereum node
+  const chainId = await web3.eth.getChainId();
+  // Load chain information over an HTTP API
+  const chainData = await EvmChains.getChain(chainId);
+  document.querySelector("#network-name").textContent = chainData.name;
+
   // Get list of accounts of the connected wallet
-  const accounts = await web3
-    .eth
-    .getAccounts();
+  const accounts = await web3.eth.getAccounts();
+
+  // MetaMask does not give you all accounts, only the selected account
+  console.log("Got accounts", accounts);
+  selectedAccount = accounts[0];
+
+  document.querySelector("#selected-account").textContent = selectedAccount;
 
   // Get a handl
-  const template = document.querySelector('#template-balance');
+  const template = document.querySelector("#template-balance");
   const accountContainer = document.querySelector("#accounts");
 
+  // Purge UI elements any previously loaded accounts
+  accountContainer.innerHTML = '';
+
   // Go through all accounts and get their ETH balance
-  accounts.map(address => {
+  const rowResolvers = accounts.map(async (address) => {
     const balance = await web3.eth.getBalance(address);
-
+    // ethBalance is a BigNumber instance
+    // https://github.com/indutny/bn.js/
+    const ethBalance = web3.utils.fromWei(balance, "ether");
+    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
     const clone = template.content.cloneNode(true);
-
-    clone
-      .querySelectorAll(".address")
-      .textContent = address;
-
-    clone
-      .querySelectorAll(".balance")
-      .textContent = balance;
-
+    clone.querySelector(".address").textContent = address;
+    clone.querySelector(".balance").textContent = humanFriendlyBalance;
     accountContainer.appendChild(clone);
   });
+  await Promise.all(rowResolvers);
+
+  document.querySelector("#prepare").style.display = "none";
+  document.querySelector("#connected").style.display = "block";
+}
+
+
+/**
+ * Connect wallet button pressed.
+ */
+async function onConnect() {
+  // Get a live wallet provider
+  console.log("Opening a dialog");
+  provider = await web3Modal.connect();
+
+  // Disable button while UI is loading.
+  // fetchAccountData() will take a while as it communicates
+  // with Ethereum node via JSON-RPC and loads chain data
+  // over an API call.
+  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
+  await fetchAccountData(provider);
+  document.querySelector("#btn-connect").removeAttribute("disabled")
+}
+
+/**
+ * Disconnect wallet button pressed.
+ */
+async function onDisconnect() {
+
+  console.log("Killing the wallet connection", provider);
+
+  // TODO: MetamaskInpageProvider does not provide disconnect?
+  if(provider.close) {
+    await provider.close();
+    provider = null;
+  }
+
+  selectedAccount = null;
+
+  document.querySelector("#prepare").style.display = "block";
+  document.querySelector("#connected").style.display = "none";
+}
+
+
+/**
+ * Main entry point.
+ */
+window.addEventListener('load', async () => {
+  init();
+  document.querySelector("#btn-connect").addEventListener("click", onConnect);
+  document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
+});
