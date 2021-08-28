c452b8db2311787f7dfa3f97180627246cf72e04
alex.mindustry <alex.mindustry@gmail.com>
2021-08-28 15:36:33 +0800

    add all

--- example.js
+++ example.js
@@ -295,3 +295,35 @@ window.addEventListener("load", async () => {
     .querySelector("#btn-disconnect")
     .addEventListener("click", onDisconnect);
 });
+
+const ethereumButton = document.querySelector('.enableEthereumButton');
+const sendEthButton = document.querySelector('.sendEthButton');
+
+let accounts2 = [];
+
+//Sending Ethereum to an address
+sendEthButton.addEventListener('click', () => {
+  ethereum
+    .request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: accounts2[0],
+          to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+          value: '0x29a2241af62c0000',
+          gasPrice: '0x09184e72a000',
+          gas: '0x2710',
+        },
+      ],
+    })
+    .then((txHash) => console.log(txHash))
+    .catch((error) => console.error);
+});
+
+ethereumButton.addEventListener('click', () => {
+  getAccount();
+});
+
+async function getAccount() {
+  accounts2 = await ethereum.request({ method: 'eth_requestAccounts' });
+}
