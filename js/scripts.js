dcc8334808e5e8a0ba396cf8e69706cc29f0f37a
alex.mindustry <alex.mindustry@gmail.com>
2021-09-13 22:37:56 +0800

    add

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector("#mainNav");
+    if (mainNav) {
+      new bootstrap.ScrollSpy(document.body, {
+        target: "#mainNav",
+        offset: 74
+      });
+    }
+  
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
+    const responsiveNavItems = [].slice.call(
+      document.querySelectorAll("#navbarResponsive .nav-link")
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener("click", () => {
+        if (window.getComputedStyle(navbarToggler).display !== "none") {
+          navbarToggler.click();
+        }
+      });
+    });
+  });
