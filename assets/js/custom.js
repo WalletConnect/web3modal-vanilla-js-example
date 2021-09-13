60761b4d54021ef076ebce3d915aa76f976b2005
alex.mindustry <alex.mindustry@gmail.com>
2021-09-13 22:59:03 +0800

    add imgs

---
+++ assets/js/custom.js
@@ -0,0 +1,55 @@
+﻿/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            //SCROLL SCRIPT
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+		//scrollReveal scripts
+            window.scrollReveal = new scrollReveal();
+            
+
+            /*====================================
+            WRITE YOUR SCRIPTS BELOW
+            ======================================*/
+
+
+
+
+
+
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
